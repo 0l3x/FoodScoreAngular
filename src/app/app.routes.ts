bcd7e875a56248ea0de4e4ee914e51d518fa2703
0l3x <olexanderg3@gmail.com>
2025-04-11 19:36:20 +0200

    menu, rutas implementado, logout, login, funcional todo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { loginActivateGuard } from './shared/guards/login-activate.guard';
+import { logoutActivateGuard } from './shared/guards/logout-activate.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [logoutActivateGuard],
     loadChildren: () => import('./auth/auth.routes').then((m) => m.authRoutes),
   },
   {
     path: 'restaurants',
+    canActivate: [loginActivateGuard],
     loadChildren: () =>
       import('./restaurants/restaurant.routes').then(
         (m) => m.restaurantsRoutes
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
+
+@Component({
+  selector: 'menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent {
+  #authService = inject(AuthService);
+  #router = inject(Router);
+  showMenu = this.#authService.getLogged();
+
+  logout() {
+    this.#authService.logout();
+    this.#router.navigate(['/auth/login']);
+  }
+}
